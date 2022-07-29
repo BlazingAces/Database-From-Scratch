e94ddefac352b4c3b825dffb116ef86775e033dd
BlazingAces <1002charles@gmail.com>
2022-07-29 12:56:01 -0400

    Server.js updated for Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { Pool } = require('pg');
 const cors = require("cors")
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 
 //MiddleWare========================================================================
