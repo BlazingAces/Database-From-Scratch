ed79a436ea7bfbd5daf97fad65263c6523678b7d
BlazingAces <1002charles@gmail.com>
2022-07-29 15:10:16 -0400

    Added .env file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const fs = require('fs');
 const { Pool } = require('pg');
+const dotenv = require("dotenv")
 const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT;
@@ -13,17 +14,24 @@ const PORT = process.env.PORT;
 app.use(express.static("static"));
 app.use(express.json());
 app.use(cors())
+dotenv.config();
 
 //Connect Database==================================================================
 
+// const dbConfig= {
+//   connectionString: process.env.DATABASE_URL,
+// }
+//  if(process.env.NODE_ENV === 'production') {
+//   dbConfig.ssl = {
+//     rejectUnauthorized: false,
+//   }
+// }
     const pool = new Pool ({
         connectionString: process.env.DATABASE_URL,
         user: '1002c',
         database: 'Metroid',
         password: 'Zelda@1002',
-        ssl: {
-          rejectUnauthorized: false
-        }
+        ssl: NODE_ENV === "production" ? {rejectUnauthorized: false} : false
     })
 
 //GET===============================================================================
